2cbec34a08e26bc4d4b4d26b561829367a77bfbc
David Fisher <davie.fisher@gmail.com>
2016-03-09 17:53:11 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var generalAssembly1 = ["Singh", "Ilya", "Lisa", "Sean", "Erik"]
+
+var generalAssembly2 = ["Christina", "David", "Evan", "Carlos", "Allie"]
+
+var index = 0;
+
+for (var i = 0; i < generalAssembly1.length; i++) {
+  console.log(generalAssembly1[i]);
+}
+
+while( index < generalAssembly2.length) {
+  console.log(generalAssembly2[index]);
+  index++;
+}
